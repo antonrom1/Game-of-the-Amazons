39bdcc66a92f5febc5d008b72d48360bd817694b
Anton Romanova <antonrom1@users.noreply.github.com>
2021-04-06 18:43:21 +0200

    fix de main thread executor

--- src/controllers/main_thread_executor.py
+++ src/controllers/main_thread_executor.py
@@ -5,27 +5,24 @@ class MainThreadExecutor(QObject):
     shared = None
     _signal = pyqtSignal()
 
-    def __init__(self):
-        super(MainThreadExecutor, self).__init__()
-        self._signal.connect(self._run)
-
-    def run(self, f, *args, **kwargs):
-        self.main_thread_func = f, args, kwargs
-        self._signal.emit()
-
-    def _run(self):
-        if self.main_thread_func is None:
-            return
-
-        func, args, kwargs = self.main_thread_func
-        self.main_thread_func = None
-
-        func(*args, **kwargs)
-
     @classmethod
     def init_shared_executor(cls):
         if cls.shared is None:
             cls.shared = MainThreadExecutor()
+
+    def __init__(self):
+        super(MainThreadExecutor, self).__init__()
+        self.main_thread_func = []
+        self._signal.connect(self._run)
+
+    def run(self, f, *args, **kwargs):
+        self.main_thread_func.append((f, args, kwargs))
+        self._signal.emit()
+
+    def _run(self):
+        while self.main_thread_func:
+            func, args, kwargs = self.main_thread_func.pop(0)
+            func(*args, **kwargs)
 
 
 MainThreadExecutor.init_shared_executor()

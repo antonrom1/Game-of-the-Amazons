6329cbbb215d9ae89cad7d2b9b073b789598ad8b
Anton Romanova <antonrom1@users.noreply.github.com>
2021-04-06 18:38:34 +0200

    changement de noms de variables et clarifications

--- src/controllers/new_game_view_controller.py
+++ src/controllers/new_game_view_controller.py
@@ -1,18 +1,16 @@
 from src.models.amazons import Amazons
-from src.views import new_game_settings
-from src.const import PLAYERS, AI_AI_DELAY_DEFAULT
-from src.models.players import AIPlayer
-from src.controllers.game_view_controller import HumanGuiPlayer, AIGuiPlayer
+from src.views.new_game_settings import NewGameSettings, NewGameSettingsDelegate
+from src.const import PLAYERS, AI_AI_DELAY_DEFAULT_MILLIS
+from src.controllers.player_ui import HumanGuiPlayer, AIGuiPlayer
 from src.views.const_strings import HUMAN_PLAYER, AI_PLAYER
-from abc import ABC, abstractmethod
 
 
-class NewGameViewController(new_game_settings.NewGameSettingsDelegate):
+class NewGameViewController(NewGameSettingsDelegate):
     def __init__(self, delegate):
-        self.new_game_settings_view = new_game_settings.NewGameSettings(self, PLAYERS)
+        self.new_game_settings_view = NewGameSettings(self, PLAYERS)
         self.new_game_settings_view.show()
 
-        self.ai_ai_delay = AI_AI_DELAY_DEFAULT
+        self.ai_ai_delay = AI_AI_DELAY_DEFAULT_MILLIS
         self.game = None
 
         self.delegate = delegate
@@ -26,27 +24,26 @@ class NewGameViewController(new_game_settings.NewGameSettingsDelegate):
 
     def save_settings(self, file_path, players_str, ai_ai_delay) -> bool:
         try:
-            self.game = Amazons(file_path, self.ai_ai_delay)
+            self.game = Amazons(file_path, show_text_board=False)
         except Exception:
             # le fichier plateau n'est pas correctement défini
             return False
         else:
             players = []
+            ai_ai_delay = ai_ai_delay if all(player == AI_PLAYER for player in players_str) else 0
             for i, player_str in enumerate(players_str):
                 if player_str == HUMAN_PLAYER:
-                    cls = HumanGuiPlayer
+                    player = HumanGuiPlayer(self.game.board, PLAYERS[i])
                 elif player_str == AI_PLAYER:
-                    cls = AIGuiPlayer
+                    player = AIGuiPlayer(ai_ai_delay, self.game.board, PLAYERS[i])
                 else:
                     raise ValueError("players_str ne correspondent pas aux types de joueurs définis")
-                player = cls(self.game.board, PLAYERS[i])
                 players.append(player)
             self.game.players = players
             self.delegate.new_game_created(self.game)
             return True
 
 
-class NewGameViewControllerDelegate(ABC):
-    @abstractmethod
+class NewGameViewControllerDelegate:
     def new_game_created(self, game: Amazons):
         pass

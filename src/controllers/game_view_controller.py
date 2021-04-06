88cedc489cb58f6ed9e41508c1eb8c0a1e47b082
Anton Romanova <antonrom1@users.noreply.github.com>
2021-04-06 18:35:56 +0200

    séparation des gui_players dans un fichier séparé

--- src/controllers/game_view_controller.py
+++ src/controllers/game_view_controller.py
@@ -1,121 +1,13 @@
-from src.models.amazons import Amazons
-from src.models.pos2d import Pos2D
+import threading
+from src.controllers.player_ui import GuiPlayerDelegate, HumanGuiPlayer
+from src.models.amazons import Amazons, AmazonsDelegate
 from src.views.board_view import BoardView
 from src.views.board_scene import BoardDelegate
-from src.views.window import MainWindow
-from src.models.action import Action
-from src.models.players import Player, AIPlayer
-import threading
-from src.const import PLAYER_1, PLAYER_2
+from src.views.window import GameWidget, GameWidgetDelegate
+from src.controllers.point_pos_bridge import to_ui_coord, action_to_game_coords
 from src.controllers.main_thread_executor import MainThreadExecutor
+from copy import deepcopy
 
-
-class HumanGuiPlayer(Player, BoardDelegate):
-    def __init__(self, board, player_id):
-        super(HumanGuiPlayer, self).__init__(board, player_id)
-        self.board_scene = None
-        self.can_act = False
-        self.action = None
-        self.delegate = None
-
-    def _play(self):
-        self.can_act = True
-        self.action = None
-        self.board_scene.allow_pieces_interaction(self.player_id)
-
-        # wait until the user moves his pieces
-        while self.action is None:
-            pass
-
-        self.can_act = False
-        MainThreadExecutor.shared.run(self.delegate.played)
-
-        return self.action
-
-
-    def piece_selected(self, coord) -> [tuple]:
-        queen_coord = to_game_coord(self.board.size, coord)
-        moves = list(self.board._possible_moves_from(queen_coord))
-        return [to_ui_coord(self.board.size, m) for m in moves]
-
-    def piece_moved(self, from_coord, to_coord) -> [tuple]:
-        queen_init_coords = to_game_coord(self.board.size, from_coord)
-        queen_final_coord = to_game_coord(self.board.size, to_coord)
-
-        possible_arr_positions = list(self.board._possible_moves_from(queen_final_coord, queen_init_coords))
-
-        return [to_ui_coord(self.board.size, pos) for pos in possible_arr_positions]
-
-    def is_action_valid(self, queen_from, queen_to, arr) -> bool:
-        if not self.can_act:
-            return False
-        queen_from_bd = to_game_coord(self.board.size, queen_from)
-        queen_to_bd = to_game_coord(self.board.size, queen_to)
-        arr_bd = to_game_coord(self.board.size, arr)
-
-        try:
-            player = self.board.grid[queen_from_bd]
-        except IndexError:
-            print("Invalid queen position")
-            return False
-
-        try:
-            action = Action(queen_from_bd, queen_to_bd, arr_bd, player)
-        except Exception:
-            print("This is not an action")
-            return False
-
-        try:
-            self.board.act(action)
-        except Exception:
-            print("Action is invalid")
-            return False
-        else:
-            print("Action is valid")
-            self.board.undo()
-            return True
-
-    def perform_action(self, queen_from, queen_to, arr):
-        self.action = Action(*[to_game_coord(self.board.size, pos)
-                               for pos in (queen_from, queen_to, arr)], self.player_id)
-        MainThreadExecutor.shared.run(self.delegate.switch_to_next_player, self.other_player_id)
-
-
-class AIGuiPlayer(AIPlayer, BoardDelegate):
-    def __init__(self, *args, **kwargs):
-        super(AIGuiPlayer, self).__init__(*args, **kwargs)
-        self.delegate = None
-        self.can_return_action = False
-
-    def _play(self):
-        action = super(AIGuiPlayer, self)._play()
-        self.can_return_action = False
-        MainThreadExecutor.shared.run(self.delegate.played, action)
-        while not self.can_return_action:
-            pass
-        return action
-
-    def piece_selected(self, coord) -> [tuple]:
-        return []
-
-    def piece_moved(self, from_coord, to_coord) -> [tuple]:
-        return []
-
-    def is_action_valid(self, queen_from, queen_to, arr) -> bool:
-        return True
-
-    def perform_action(self, queen_from, queen_to, arr):
-        MainThreadExecutor.shared.run(self.delegate.switch_to_next_player, self.other_player_id)
-        self.can_return_action = True
-
-
-
-class GuiPlayerDelegate:
-    def played(self, action):
-        raise NotImplemented
-
-    def switch_to_next_player(self, next_player_id):
-        raise NotImplemented
 
 
 class GameViewController(BoardDelegate, GuiPlayerDelegate):
---
+++ src/controllers/player_ui.py
@@ -0,0 +1,128 @@
+from src.controllers.point_pos_bridge import to_game_coord, to_ui_coord
+from src.models.action import Action
+from src.models.players import Player, AIPlayer
+from src.views.board_scene import BoardDelegate
+from time import time
+
+
+class GuiPlayer(Player):
+    def __init__(self, board, player_id):
+        super(GuiPlayer, self).__init__(board, player_id)
+        self.board_scene = None
+        self.action = None
+        self.delegate = None
+        self.can_act = False
+
+        self.should_stop_execution = False
+
+    def play(self):
+        action = self._play()
+        if not self.should_stop_execution:
+            self.board.act(action)
+
+
+class HumanGuiPlayer(GuiPlayer, BoardDelegate):
+    def __init__(self, board, player_id):
+        super(HumanGuiPlayer, self).__init__(board, player_id)
+        self.action = None
+
+    def _play(self):
+        self.can_act = True
+        self.action = None
+        self.board_scene.allow_pieces_interaction(self.player_id)
+
+        # wait until the user moves his pieces
+        while self.action is None and not self.should_stop_execution:
+            pass
+        self.can_act = False
+        self.delegate.played()
+
+        return self.action
+
+    def piece_selected(self, coord) -> [tuple]:
+        queen_coord = to_game_coord(self.board.size, coord)
+        moves = list(self.board._possible_moves_from(queen_coord))
+        return [to_ui_coord(self.board.size, m) for m in moves]
+
+    def piece_moved(self, from_coord, to_coord) -> [tuple]:
+        queen_init_coords = to_game_coord(self.board.size, from_coord)
+        queen_final_coord = to_game_coord(self.board.size, to_coord)
+
+        possible_arr_positions = list(self.board._possible_moves_from(queen_final_coord, queen_init_coords))
+
+        return [to_ui_coord(self.board.size, pos) for pos in possible_arr_positions]
+
+    def is_action_valid(self, queen_from, queen_to, arr) -> bool:
+        if not self.can_act:
+            return False
+        queen_from_bd = to_game_coord(self.board.size, queen_from)
+        queen_to_bd = to_game_coord(self.board.size, queen_to)
+        arr_bd = to_game_coord(self.board.size, arr)
+
+        try:
+            player = self.board.grid[queen_from_bd]
+        except IndexError:
+            print("Invalid queen position")
+            return False
+
+        try:
+            action = Action(queen_from_bd, queen_to_bd, arr_bd, player)
+        except Exception:
+            print("This is not an action")
+            return False
+
+        try:
+            self.board.act(action)
+        except Exception:
+            print("Action is invalid")
+            return False
+        else:
+            # l'action est valide!
+            self.board.undo()
+
+            return True
+
+    def perform_action(self, queen_from, queen_to, arr):
+        self.action = Action(*[to_game_coord(self.board.size, pos)
+                               for pos in (queen_from, queen_to, arr)], self.player_id)
+
+
+class AIGuiPlayer(GuiPlayer, AIPlayer, BoardDelegate):
+    def __init__(self, ai_ai_delay, *args, **kwargs):
+        super(AIGuiPlayer, self).__init__(*args, **kwargs)
+        self.ai_ai_delay = ai_ai_delay
+        self.action_start_time = None
+
+        self.can_return_action = False
+
+    def _play(self):
+        self.action_start_time = time()
+        self.board_scene.stop_all_possible_interaction()
+        action = super(AIGuiPlayer, self)._play()
+        self.can_return_action = False
+
+        while not (time() - self.action_start_time >= self.ai_ai_delay or
+                   self.should_stop_execution):
+            pass
+        self.delegate.played(action)
+        while not (self.can_return_action or self.should_stop_execution):
+            pass
+        return action
+
+    def piece_selected(self, coord) -> [tuple]:
+        return []
+
+    def piece_moved(self, from_coord, to_coord) -> [tuple]:
+        return []
+
+    def is_action_valid(self, queen_from, queen_to, arr) -> bool:
+        return True
+
+    def perform_action(self, queen_from, queen_to, arr):
+        # attendre que la gui ait fini l'animation avant d'appliquer le coup à Amazons
+        self.can_return_action = True
+
+
+class GuiPlayerDelegate:
+    def played(self, action):
+        raise NotImplemented

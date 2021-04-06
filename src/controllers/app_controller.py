b8e8305b6cf41323dab3d424176844faf9232b59
Anton Romanova <antonrom1@users.noreply.github.com>
2021-04-06 18:34:21 +0200

    ajout de variables d'environnement pour pygame et qt

--- src/controllers/app_controller.py
+++ src/controllers/app_controller.py
@@ -1,16 +1,20 @@
 from src.views.app import AmazkombatApp
 from src.controllers.new_game_view_controller import NewGameViewController, NewGameViewControllerDelegate
 from src.controllers.game_view_controller import GameViewController
+from src.views.sound import AmazonsSound
+from src.const import XDG_RUNTIME_DIR_ENVIRON
+from os import environ
 
 
 class AppController(NewGameViewControllerDelegate):
     def __init__(self):
+        environ.update(XDG_RUNTIME_DIR_ENVIRON)
         self.app = AmazkombatApp()
 
         NewGameViewController(self)
         self.game_controller = None
-
         self.app.exec_()
+
 
     def new_game_created(self, game):
         self.game_controller = GameViewController(game)
